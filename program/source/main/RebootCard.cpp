2a15e6430333735b545055ec2d8a29efeb151fc5
SoreruSushi <sushigmpad64@outlook.jp>
2026-01-24 16:12:45 +0900

    UE削除

--- program/source/main/RebootCard.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include <main/RebootCard.h>
-#include "Modules/ModuleManager.h"
-
-#include <database/type_id.inc>
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RebootCard, "RebootCard" );
